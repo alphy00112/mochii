f3ff757893e8641ba7f7e532650b2264584d1753
alphy00112 <56547174+alphy00112@users.noreply.github.com>
2023-03-01 16:02:46 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,80 @@
+/**
+   * Create By Dika Ardnt.
+   * Recode By Naze Dev
+   * Contact Me on wa.me/628116646665
+   * Recode : Naze & YangJunMing12
+*/
+
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': 'woaibeijingtiananmen',
+}
+
+// Other
+global.owner = ['6281328433068']
+global.ownernomer = "6281328433068"
+global.premium = ['6281328433068']
+global.packname = 'Made By'
+global.author = 'MochiBot V2.05'
+global.sessionName = 'nazedev'  //jangan diganti bro nanti error
+global.jumlha = '999'
+global.jumhal = '100000000000000'
+global.jumlah = '1000000000'
+global.prefa = ['','!','.','#','&']
+global.sp = ''
+
+// Setting Mess
+global.mess = {
+    success: '✅Done',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    premime: 'Fitur Khusus Premium Kalo Mau Daftar Ketik Sewa / Chat Owner',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    error: 'Error!',
+    errapi: 'Error Mungkin Apikey Tidak Valid!',
+    errmor: 'Error Kesalahan Sistem',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+
+// Limit
+global.limitawal = {
+    premium: "Infinity",
+    free: 25
+}
+
+// Fake
+global.thumb = fs.readFileSync('./media/image/naze.jpg')
+global.faall = fs.readFileSync('./media/image/fake.jpg')
+
+// Url
+global.mygit = 'https://wa.famsmochi.cloud'
+global.myyt = 'Yt kamu'
+global.myytv = 'Yt video kamu'
+global.mygc = "Grup wa kamu"
+
+// Engak Ngaruh, Belum Kepasang
+global.botname = 'Mochi-BOT'
+global.akulaku = 'Bot by Kisanagi'
+global.ytname = 'Nama yt kamu' //Kalo Ngk Punya Yt Ngak Usah Di Ganti
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
